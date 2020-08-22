4a2924d76defa1d628308550467c837f73e975b3
Normisss11 <69723335+normacalamartinez@users.noreply.github.com>
2020-08-21 20:41:21 -0500

    Experiencia de compra

---
+++ Compras/Librerias_Compras.py
@@ -0,0 +1,54 @@
+
+               #################### LIBRERÍAS DEL PROGRAMA DE COMPRAS #################### 
+
+"""
+Created on Fri Jul 01 18:49:02 2020
+@author: Norma Cala
+"""
+
+# Almacen de funciones listas para ser llamadas en las librerias.
+
+def iva(valor,iva = 0.16):
+    valor = valor*iva
+    return valor
+
+def ica(valor, ica = 0.2):
+    valor = valor*ica
+    return valor
+
+def getDescuento(edad):
+    
+            if(edad > 18 and edad <= 25):                
+                return 0.10
+            elif(edad > 25 and edad <= 40):
+                return 0.15
+            else:
+                return 0.20
+ 
+    
+def getNombre():
+    return input('  Ingrese nombre ==> ')
+
+def getValorCompra():
+        while True:            
+            try:
+                return float(input('  Valor compra ==> $ '))
+            except Exception as error:
+                print('  Datos erróneos',error)  
+
+def getdescuentazo():
+    while True:
+        try:
+            descuento = getDescuento(int(input('  Digite edad ==> ')))          
+            return descuento
+        except Exception as error:
+             print('  Datos erróneos',error)
+            
+
+            
+#def getvalorapagar():    
+ #   valor_por_pagar = getValorCompra()-getValorCompra()*getdescuentazo() + iva(getValorCompra)
+ #   return valor_por_pagar
+    
+    
+    
